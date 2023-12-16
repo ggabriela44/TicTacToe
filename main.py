fa3683d943f700d6933dbd8e46c5ae3d0b869488
Izagg <iza440@onet.eu>
2023-12-16 18:16:38 +0100

    Dodanie lini w polu gry.

--- main.py
+++ main.py
@@ -1,38 +1,27 @@
 import pygame
+import settings as s
 
 pygame.init()
 
-#window config
-SIZESCREEN = WIDTH, HEIGHT = 600, 550
-screen = pygame.display.set_mode(SIZESCREEN)
-pygame.display.set_caption("TicTacToe")
-
-
-#framerate
-FPS = 60
-clock = pygame.time.Clock()
-
-
-bg_image = pygame.image.load("./images/background_wood.png").convert_alpha()
-icon = pygame.image.load("./images/bamboo_X.png").convert_alpha()
-
-
 #icon config
-pygame.display.set_icon(icon)
+pygame.display.set_icon(s.icon)
 
 #font config
 font = pygame.font.SysFont("Courier New", 40)
-
 
 #main game loop
 run = True
 while run:
 
-    clock.tick(FPS)
+    s.clock.tick(s.FPS)
 
     #draw background
-    scaled_bg = pygame.transform.scale(bg_image, SIZESCREEN)
-    screen.blit(scaled_bg, (0, 0))
+    scaled_bg = pygame.transform.scale(s.bg_image, s.SIZESCREEN)
+    s.screen.blit(scaled_bg, (0, 0))
+    s.screen.blit(s.stick1, (50, 100))
+    s.screen.blit(s.stick2, (450, 100))
+    s.screen.blit(s.stickh1, (60, 100))
+    s.screen.blit(s.stickh2, (60, 450))
 
     #event handler
     for event in pygame.event.get():
